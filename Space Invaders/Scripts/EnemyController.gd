c9ca923fb2ce1420e437e28f6ccbfa8839184140
MisterLs4566 <85504773+MisterLs4566@users.noreply.github.com>
2023-07-25 20:32:40 +0200

    update

--- Space Invaders/Scripts/EnemyController.gd
+++ Space Invaders/Scripts/EnemyController.gd
@@ -9,3 +9,8 @@ func _process(delta):
 	if position.distance_to(player.position) < 1000:
 		look_at(player.position)
 		rotation_degrees += 90
+
+
+func _on_AnimatedSprite_animation_finished():
+	if  $AnimatedSprite.get_animation() == "Explosion":
+		queue_free()
--- Space Invaders/Scripts/LaserController.gd
+++ Space Invaders/Scripts/LaserController.gd
@@ -11,9 +11,14 @@ var audioPlayer
 var collision
 var collisionCollider
 
+var laserSound
+var explosionSound
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	player = get_node("../Player")
+	laserSound = preload("res://Sounds/Laser1FireSound.wav")
+	explosionSound = preload("res://Sounds/Laser1ExplosionSound.wav")	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
@@ -22,19 +27,26 @@ func _process(delta):
 	if isShooting == true:
 		if position.distance_to(oldPosition) > maxDistance:
 				velocity = Vector2.ZERO
+				$AudioStreamPlayer2D.stream = explosionSound
+				$AudioStreamPlayer2D.play()
 				$AnimatedSprite.play("Explosion")
+				return
 	if get_slide_count() != 0:
 		for i in range(0, get_slide_count()):
 				collision = get_slide_collision(i).collider as KinematicBody2D
 				collisionCollider = get_slide_collision(i).collider as CollisionObject2D
 				
 				if collisionCollider.collision_layer == 2:
-					collision.queue_free()
+					collision.get_child(0).play("Explosion")
 					velocity = Vector2.ZERO
+					$AudioStreamPlayer2D.stream = explosionSound
+					$AudioStreamPlayer2D.play()
 					$AnimatedSprite.play("Explosion")
 
 func _on_Player_shoot():
 	if isShooting == false:
+		$AudioStreamPlayer2D.stream = laserSound
+		$AudioStreamPlayer2D.play()
 		position = player.position
 		oldPosition = position
 		isShooting = true
