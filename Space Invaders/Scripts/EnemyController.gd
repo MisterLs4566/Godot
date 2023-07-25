111722b8a5d68d97b5f1a2fbc40dee25dfa21a8e
MisterLs4566 <85504773+MisterLs4566@users.noreply.github.com>
2023-07-25 15:15:16 +0200

    update

--- Space Invaders/Scripts/EnemyController.gd
+++ Space Invaders/Scripts/EnemyController.gd
@@ -1,11 +1,11 @@
-extends Node
+extends KinematicBody2D
 
+var player
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	player = get_node("../Player")
+
+func _process(delta):
+	if position.distance_to(player.position) < 1000:
+		look_at(player.position)
+		rotation_degrees += 90
--- Space Invaders/Scripts/PlayerController.gd
+++ Space Invaders/Scripts/PlayerController.gd
@@ -1,56 +1,25 @@
 extends KinematicBody2D
 
 var speed = 500
-var velocity = Vector2()
-var rot = 1
+var velocity = Vector2(0, -1)
 signal shoot
 signal killed
 var slowDown = false
+var start = false
 
 func _ready():
 	pass
 	
 func input():
-	if Input.is_action_just_pressed("ui_right"):
-		rotation_degrees += 45
-		if rot < 8:
-			rot += 1
-		else:
-			rot = 1
-	if Input.is_action_just_pressed("ui_left"):
-		rotation_degrees -= 45
-		if rot > 1:
-			rot -= 1
-		else:
-			rot = 8
 	if Input.is_action_just_pressed("ui_up"):
 		slowDown = false
+		start = true
+		"""Richtung setzen"""
+		velocity.x = 0
+		velocity.y = -1
+		velocity = velocity.rotated(rotation).normalized()
 		"""Animation setzen"""
 		$AnimatedSprite.play("Movement")
-		if rot == 1:
-			 velocity.x = 0 
-			 velocity.y = -1
-		elif rot == 2:	
-			velocity.x = 1
-			velocity.y = -1
-		elif rot == 3:
-			velocity.x = 1
-			velocity.y = 0
-		elif rot == 4:
-			velocity.x = 1
-			velocity.y = +1
-		elif rot == 5:	
-			velocity.x = 0
-			velocity.y = +1
-		elif rot == 6:
-			velocity.x = -1
-			velocity.y = +1
-		elif rot == 7:
-			velocity.x = -1
-			velocity.y = 0
-		elif rot == 8:	
-			velocity.x = -1
-			velocity.y = -1
 	if Input.is_action_just_pressed("ui_down"):
 		$AnimatedSprite.play("Idle")
 		slowDown = true
@@ -62,7 +31,12 @@ func input():
 	
 func _process(delta):
 	input()
+	look_at(get_global_mouse_position())
+	rotation_degrees += 90
+	if start == false:
+		return
+		
 	if slowDown == false:
-		move_and_slide(velocity.normalized() * speed)
+		move_and_slide(velocity* speed)
 	else:
-		move_and_slide(velocity.normalized() * speed/2)
+		move_and_slide(velocity* speed/2)
