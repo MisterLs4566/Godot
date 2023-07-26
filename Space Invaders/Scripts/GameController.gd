9a49ba557fe9fb0c978479ce2f52dba7599a75eb
MisterLs4566 <85504773+MisterLs4566@users.noreply.github.com>
2023-07-26 11:40:13 +0200

    update

--- Space Invaders/Scripts/GameController.gd
+++ Space Invaders/Scripts/GameController.gd
@@ -11,4 +11,4 @@ func _ready():
 	#OS.window_fullscreen = true
 	VisualServer.set_default_clear_color(Color(0, 0, 0))
 	"""Lautstärke"""
-	#AudioServer.set_bus_volume_db(AudioServer.get_bus_index("GameSounds"), -40)
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("GameSounds"), -40)
