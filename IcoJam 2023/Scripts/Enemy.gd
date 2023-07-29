ae5b6016d22dff54e9eeb152c3658f0b7332ee10
MisterLs4566 <85504773+MisterLs4566@users.noreply.github.com>
2023-07-29 11:27:48 +0200

    update

--- IcoJam 2023/Scripts/Enemy.gd
+++ IcoJam 2023/Scripts/Enemy.gd
@@ -2,10 +2,15 @@ extends KinematicBody2D
 
 var gravity = 900
 var state = 0
+var type
 
 func _ready():
-	pass
-	
+	if get_groups()[0] == "Circle":
+		type = "circle"
+	elif get_groups()[0] == "Rect":
+		type = "rect"
+	elif get_groups()[0] == "Heart":
+		type = "heart"
 func _process(delta):
 	move_and_slide(Vector2.DOWN * gravity * state)
 
@@ -13,6 +18,5 @@ func _process(delta):
 func _on_Node2D_resume():
 	state = 1
 
-
 func _on_Node2D_gameStart():
-	state = 0
+	state = 1
--- IcoJam 2023/Scripts/GameController.gd
+++ IcoJam 2023/Scripts/GameController.gd
@@ -11,12 +11,11 @@ var rect
 var heart
 
 func _ready():
-	
 	VisualServer.set_default_clear_color(Color("#2697f0"))
-	emit_signal("startTimer")
-	circle = preload("res://Prefabs/Circle.tscn")
+	circle = load("res://Prefabs/Circle.tscn")
 	rect = preload("res://Prefabs/Rect.tscn")
 	heart = preload("res://Prefabs/Heart.tscn")
+	emit_signal("startTimer")
 	
 func _process(delta):
 	pass
@@ -24,21 +23,23 @@ func _process(delta):
 """Timer und Spielpausierung"""
 
 func _on_Node2D_startTimer():
-	if level == 0:
-		emit_signal("gameStart")
-		level += 1
 	$Timer.wait_time = 3.0
 	$Timer.start()
 	
 """Wenn 3 Sekunden Timer abgelaufen"""
 func _on_Timer_timeout():
-	emit_signal("resume")
+	if(level == 0):
+		level += 1
+		emit_signal("gameStart")
+	else:
+		emit_signal("resume")
 
 
 """Wenn das Spiel startet"""
 func _on_Node2D_gameStart():
-	print("test")
 	circleObject = circle.instance()
-	circleObject.position = Vector2(0, 0)
-	circleObject.connect("gameStart", self, "_on_Node2D_gameStart")
-	circleObject.connect("resume", self, "_on_Node2D_resume")
+	$GameBorder.add_child(circleObject)
+	circleObject.position = Vector2(0, -900)
+	connect("gameStart", circleObject, "_on_Node2D_gameStart")
+	connect("resume", circleObject, "_on_Node2D_resume")
+	emit_signal("resume")
