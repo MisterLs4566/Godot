1dc5debab3f6ea8b3c98b39bcec01306afd130e0
MisterLs4566 <85504773+MisterLs4566@users.noreply.github.com>
2023-08-03 01:01:43 +0200

    update

--- Asteroids (2023)/Scripts/LaserController.gd
+++ Asteroids (2023)/Scripts/LaserController.gd
@@ -6,14 +6,15 @@ var player
 
 """variables"""
 
-var speed = 0
-var velocity = Vector2()
+var speed = 2000
+var velocity = Vector2(0, -1)
 var oldPosition = Vector2()
-var maxDistance = 350
+var maxDistance = 400
+
 
 """switches"""
 
-var isShooting = false
+var isShooting = true
 
 """KinematicBody2D"""
 
@@ -38,54 +39,44 @@ func _ready():
 	laserSound = preload("res://Sounds/Laser1FireSound.wav")
 	explosionSound = preload("res://Sounds/Laser1ExplosionSound.wav")
 	explosionSoundEnemy = preload("res://Sounds/Enemy1Explosion.wav")
+	oldPosition = position
+	"""shoot"""
+	rotation_degrees = player.rotation_degrees
+	$AudioStream2DLaser.play()
+	$AnimatedSprite.play("default")	
 	
 	"""connect signals"""
-	
 	connect("explosion", self, "_on_Laser_explosion")
-
 func collision():
 	if get_slide_count() != 0:
 		for i in range(0, get_slide_count()):
-				collision = get_slide_collision(i).collider as KinematicBody2D
-				collisionCollider = get_slide_collision(i).collider as CollisionObject2D
-				if collisionCollider.collision_layer == 2:
-					collision.get_child(2).play()
-					collision.get_child(0).play("Explosion")
-					collision.get_child(1).disabled = true
-					velocity = Vector2.ZERO
-					$AnimatedSprite.play("Explosion")
-					return
+			collision = get_slide_collision(i).collider as KinematicBody2D
+			collisionCollider = get_slide_collision(i).collider as CollisionObject2D
+			if collisionCollider.collision_layer == 2:
+				collision.get_child(2).play()
+				collision.get_child(0).play("Explosion")
+				collision.get_child(1).disabled = true
+				velocity = Vector2.ZERO
+				$AnimatedSprite.play("Explosion")
+				return
 
 func _process(delta):
 	move_and_slide(velocity.rotated(rotation).normalized() * speed)
 	collision()
 	if isShooting == true:
 		if (position.distance_to(oldPosition) > maxDistance):
-			"""Stoppen wenn am Explodieren?"""
-			#velocity = Vector2.ZERO
+			isShooting = false
 			$AnimatedSprite.play("Explosion")
 			emit_signal("explosion")
-			
-func _on_Player_shoot():
-	if isShooting == false:
-		$AudioStream2DLaser.play()
-		$AnimatedSprite.play("default")	
-		position = player.position
-		oldPosition = position
-		isShooting = true
-		speed = 2000
-		rotation_degrees = player.rotation_degrees
-		visible = true
-		velocity.y = -1
 
 func _on_AnimatedSprite_animation_finished():
-	if isShooting == true and $AnimatedSprite.get_animation() == "Explosion":
-		isShooting = false
-		velocity = Vector2.ZERO
-		visible = false
-		$AnimatedSprite.play("default")	
-
+	if $AnimatedSprite.get_animation() == "Explosion":
+		player.projectiles -= 1
+		if player.projectiles == 0:
+			player.laserCooldown = false
+		queue_free()
+		
 
 func _on_Laser_explosion():
-	$AudioStream2DLaserExplosion.stop()
 	$AudioStream2DLaserExplosion.play()
+	
--- Asteroids (2023)/Scripts/PlayerController.gd
+++ Asteroids (2023)/Scripts/PlayerController.gd
@@ -1,19 +1,27 @@
 extends KinematicBody2D
 
 """nodes"""
+var scene
 var laser
+var laserInstance
 var speed = 500
 var healthLabel
 
 """variables"""
-var velocity = Vector2(0, -1)
+var velocity = Vector2.ZERO
+var level
+var projectiles
 
 """switches"""
 var slowVelocity = false
 var start = false
 var hit = false
+var shootKeyPressed = false
+var laserCooldown = false
 
 var lives = 6.5
+var maxProjectiles = 3
+var cooldownSalve = 0.2
 
 """KinematicBody2D"""
 var collision
@@ -27,6 +35,8 @@ var coll
 """preloads"""
 var boostSound
 var menuScene
+var playerExplosionSound
+var playerHurtSound
 
 """signals"""
 signal shoot # -> laser
@@ -34,17 +44,41 @@ signal hurt # -> self
 signal destroyed # -> self
 
 func _ready():
-	boostSound = preload("res://Sounds/PlayerFasterSound.wav")
+	velocity = Vector2(0, -1)
+	level = 1
+	projectiles = 0
+	boostSound = preload("res://Sounds/PlayerBoostSound.wav")
 	menuScene = preload("res://Scenes/MenuScene.tscn")
+	playerExplosionSound = preload("res://Sounds/PlayerExplosionSound.wav")
+	playerHurtSound = preload("res://Sounds/PlayerHurtSound.wav")
+	laser = preload("res://Prefabs/Laser.tscn")
 	healthLabel = get_node("/root/Node2D/GameUI/HealthLabel")
-	laser = get_node("/root/Node2D/Laser")
+	scene = get_node("/root/Node2D")
 	
 	"""connect signals"""
 	
 	connect("destroyed", self, "_on_Player_destroyed")
 	connect("hurt", self, "_on_Player_hurt")
-	connect("shoot", laser, "_on_Player_shoot")
+	$CooldownTimerSalve.connect("timeout", self, "_on_CooldownTimerSalve_timeout")
+	
+func _on_CooldownTimerSalve_timeout():
+	$CooldownTimerSalve.stop()
+	if shootKeyPressed == true:
+		instanciateLaser()
+	if projectiles < maxProjectiles:
+		$CooldownTimerSalve.wait_time = cooldownSalve
+		$CooldownTimerSalve.start()
+	
+func instanciateLaser():
+	projectiles += 1
+	print(projectiles)
+	laserInstance = laser.instance()
+	laserInstance.position = position
+	scene.add_child(laserInstance)
+
 func input():
+	if Input.is_action_just_released("ui_accept"):
+		shootKeyPressed = false
 	if Input.is_action_just_pressed("ui_up"):
 		$AudioStream2DBoost.play()
 		slowVelocity = false
@@ -61,20 +95,18 @@ func input():
 			$AnimatedSprite.play("Idle")
 		slowVelocity = true
 	if Input.is_action_pressed("ui_accept"):
-		emit_signal("shoot")
+		shootKeyPressed = true
+		if projectiles < maxProjectiles:
+			if $CooldownTimerSalve.is_stopped() == true and laserCooldown == false:
+				instanciateLaser()
+				laserCooldown = true
+				$CooldownTimerSalve.wait_time = cooldownSalve
+				$CooldownTimerSalve.start()
+				
 	if Input.is_action_just_pressed("ui_end"):
 		get_tree().change_scene_to(menuScene)
-	
-func _process(delta):
-	input()
-	look_at(get_global_mouse_position())
-	rotation_degrees += 90	
-	if start == false:
-		return
-	if slowVelocity == false:
-		move_and_slide(velocity * speed)
-	else:
-		move_and_slide(velocity * speed/2)
+
+func collision():
 	if get_slide_count() != 0:
 		for i in range(0, get_slide_count()):
 			collision = get_slide_collision(i).collider as KinematicBody2D
@@ -87,9 +119,23 @@ func _process(delta):
 				emit_signal("hurt")
 			else:
 				return
+
+func _process(delta):
+	input()
+	look_at(get_global_mouse_position())
+	rotation_degrees += 90	
+	if start == false:
+		return
+	if slowVelocity == false:
+		move_and_slide(velocity * speed)
+	else:
+		move_and_slide(velocity * speed/2)
+	collision()
 				
 func _on_Player_hurt():
 	if hit == false:
+		$AudioStream2DHurt.stop()
+		$AudioStream2DHurt.play()
 		hit = true
 		lives -= 0.5
 		healthLabel.rect_scale.x = lives
@@ -106,4 +152,5 @@ func _on_AnimatedSprite_animation_finished():
 		get_tree().reload_current_scene()
 
 func _on_Player_destroyed():
+	$AudioStream2DExplosion.play()
 	$AnimatedSprite.play("Explosion")
