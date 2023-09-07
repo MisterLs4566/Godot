1501795bb56f367f93ac67c43369f7e9225f4032
MisterLs4566 <85504773+MisterLs4566@users.noreply.github.com>
2023-09-07 20:19:10 +0200

    update

--- Asteroids (2023)/Scripts/EnemyController.gd
+++ Asteroids (2023)/Scripts/EnemyController.gd
@@ -13,13 +13,18 @@ var shootPossible = false
 var maxPlayerDistance = 1000
 var lives = 3
 var fireCooldown
-var laser
+var cooldownSalve = 0.1
 
 """3 Sekunden Cooldown für den Laser, wenn Player in Sichtfeld des Enemy kommt"""
 var laserCooldown = 3
 var touchStrength = 3.5
 var laserStrength = 1
+var laserMaxDistance = 500
+var laserSpeed = 2000
 
+var projectiles = 0
+var maxProjectiles = 2
+var salveCooldown
 """signals"""
 signal explosion
 signal hurt(strength, knockbackTime, knockbackSpeed)
@@ -29,6 +34,7 @@ var laserInstance
 
 func _ready():
 	player = get_node("/root/Node2D/Player")
+	laserInstance = preload("res://Prefabs/Laser.tscn")
 	$EnemyHealthLabel.set_as_toplevel(true)
 	
 	"""connect signals"""
@@ -36,20 +42,36 @@ func _ready():
 	connect("explosion", self, "_on_Enemy_explosion")
 	connect("hurt", self, "_on_Enemy_hurt")
 	$CooldownHurtTimer.connect("timeout", self, "_on_CooldownHurtTimer_timeout")
+	$CooldownStartShootTimer.connect("timeout", self, "_on_CooldownStartShootTimer")
 func _process(delta):
 	"""update health label"""
 	$EnemyHealthLabel.rect_position.x = position.x - 40
 	$EnemyHealthLabel.rect_position.y = position.y - 160
+	
 	$EnemyHealthLabel.text = str(lives)
 	"""rotate toward player"""
 	if position.distance_to(player.position) < maxPlayerDistance:
 		look_at(player.position)
 		rotation_degrees += 90
-	
+		if shootPossible == false:
+			$CooldownStartShootTimer.wait_time = laserCooldown
+			$CooldownStartShootTimer.start()
+		else:
+			if $CooldownTimerSalve.is_stopped() == true:
+				$CooldownTimerSalve.wait_time = cooldownSalve
+				$CooldownTimerSalve.start()
+				instanciateLaser(laserInstance, laserMaxDistance, laserSpeed, laserStrength)
+				
 	"""delete if not existing"""
 	
 	if(visible == false and $Stream2DExplosion.playing == false):
 		queue_free()
+
+func instanciateLaser(laserObject, maxDistance, speed, strength):
+	pass
+	
+func _on_CooldownStartShootTimer():
+	print("shootPossible")
 
 func _on_AnimatedSprite_animation_finished():
 	if  $AnimatedSprite.get_animation() == "Hurt":
