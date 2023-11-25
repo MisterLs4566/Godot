0c27377c5029351d8e8256c2bc0ebf825280db58
MisterLs4566 <85504773+MisterLs4566@users.noreply.github.com>
2023-11-25 14:52:09 +0100

    rendering server default color

--- MyWorld/Scripts/room_controller.gd
+++ MyWorld/Scripts/room_controller.gd
@@ -6,12 +6,14 @@ func _ready():
 
 func _on_body_entered(body):
 	if body.is_in_group("Player"):
+		RenderingServer.set_default_clear_color(Color.BLACK)
 		if self.get_meta("Room") == "Playground":
 			player.level = 1
 		player.set_room(self.get_meta("Room"))
 
 
 func _on_body_exited(body):
+	RenderingServer.set_default_clear_color(Color.BISQUE)
 	if body.is_in_group("Player"):
 		player.set_room("")
 	if self.get_meta("Room") == "Playground":
